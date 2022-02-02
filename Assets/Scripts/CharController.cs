76e3284e2f0a9282cc83eebb19c8831a530a95f0
Alireza Gharib <alirezagharib110@gmail.com>
2022-02-02 12:10:01 +0330

    char controller have been updated & game controller was implemented

--- Assets/Scripts/CharController.cs
+++ Assets/Scripts/CharController.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class CharController : MonoBehaviour {
 
+    public Transform rayStart;
 
     private Rigidbody rb;
     private bool walkingRight = true;
+    private Animator anim;
+    private GameManager gameManager;
 
 
 	// Use this for initialization
 	void Awake () {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        gameManager = FindObjectOfType<GameManager>();
 	}
 
     private void FixedUpdate()
     {
+
+        if(!gameManager.gameStarted){
+            return;
+        }else{
+            anim.SetTrigger("gameStarted");
+        }
 
         rb.transform.position = transform.position + transform.forward * 2 * Time.deltaTime;
     }
@@ -27,9 +38,21 @@ public class CharController : MonoBehaviour {
             Switch();
         }
 
+        RaycastHit hit;
+
+        if(!Physics.Raycast(rayStart.position, -transform.up, out hit, Mathf.Infinity)){
+            anim.SetTrigger("isFalling");
+            Debug.Log("Falling");
+
+        }
+
+
 	}
 
     private void Switch(){
+        if(!gameManager.gameStarted){
+            return;
+        }
 
         walkingRight = !walkingRight;
 
